936d26fb0627470d5eea23a8b6334fe41ca8d8ec
ChrisCodesThings <153457755+ChrisCodesThings@users.noreply.github.com>
2024-05-01 17:07:07 +0100

    fix: nicer TS syntax and minor tweaks

--- src/colorLooksLikeRGBA.d.ts
+++ src/colorLooksLikeRGBA.d.ts
@@ -1,6 +1,6 @@
 /**
- * Determine if a set of values could be an RGB or RGBA color
- * @param {[number, number, number]|[number, number, number, number]} rgba Array containing red, green, blue and (optional) alpha values
+ * Determine if a set of values could be an RGBA color
+ * @param {[number, number, number, number|undefined]} rgba Array containing red, green, blue and (optional) alpha values
  * @returns {boolean}
  */
-export default function _default(rgba: [number, number, number] | [number, number, number, number]): boolean;
+export default function colorLooksLikeRGBA(rgba: [number, number, number, number | undefined]): boolean;
--- src/colorLooksLikeRGBA.js
+++ src/colorLooksLikeRGBA.js
@@ -1,16 +1,16 @@
 // @ts-check
 
 /**
- * Determine if a set of values could be an RGB or RGBA color
- * @param {[number, number, number]|[number, number, number, number]} rgba Array containing red, green, blue and (optional) alpha values
+ * Determine if a set of values could be an RGBA color
+ * @param {[number, number, number, number|undefined]} rgba Array containing red, green, blue and (optional) alpha values
  * @returns {boolean}
  */
-export default function (rgba) {
+export default function colorLooksLikeRGBA(rgba) {
     return (
-        (rgba.length == 3 || rgba.length == 4)
+        (rgba.length >= 3 && rgba.length <= 4)
         && rgba[0] >= 0 && rgba[0] <= 255
         && rgba[1] >= 0 && rgba[1] <= 255
         && rgba[2] >= 0 && rgba[2] <= 255
-        && (rgba.length == 4 ? rgba[3] >= 0 && rgba[3] <= 1 : true)
+        && (typeof rgba[3] !== 'undefined' ? rgba[3] >= 0 && rgba[3] <= 1 : true)
     )
 }
